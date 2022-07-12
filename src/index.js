2948a6813c05b9fc95dbe44213c1ea57d5ee2d23
Elena Kovalenko <aszhigalova@gmail.com>
2022-07-12 12:28:59 +0300

    dynamic photo cards implemented

--- src/index.js
+++ src/index.js
@@ -10,12 +10,27 @@ const photos = [
   {url: "/img/images (9).jfif", name: "photo9"},
 ];
 
-photos.forEach((photo, i) => {
-  const clone = document.querySelector(".template-card").cloneNode(true);
-  clone.querySelector("img").src = photo.url;
-  clone.querySelector("p").name = photo.name;
-  document.querySelector(".photo-container").appendChild(clone);
-});
+const addNewPhoto = (photo) => {
+      const div = document.createElement("div");
+      div.className = "template-card col-4 my-1";
+      div.innerHTML = '<div class="bg-light p-1 border rounded-3">' +
+    '<div class="card bg-light p-1 border rounded-3">' +
+    '   <img class="img-item rounded-3">' +
+    '     <div class="card-body">' +
+    '       <p class="card-text"> </p>' +
+    '<div class="d-flex justify-content-between align-items-center">' +
+    '  <div class="btn-group">' +
+    '     <button type="button" class="view-button btn btn-sm btn-outline-secondary">View</button>' +
+    '     <button type="button" class="edit-button btn btn-sm btn-outline-secondary">Edit</button>' +
+    '  </div>' +
+    '    <small class="text-muted">9 mins</small>' +
+    '</div></div></div></div>';
+    div.querySelector("img").src = photo.url;
+    div.querySelector(".card-text").innerText = photo.name;
+    document.querySelector(".photo-container").appendChild(div);
+}
+
+photos.forEach(addNewPhoto);
 
 document.querySelectorAll(".view-button").forEach(e => e.addEventListener('click', e => e.target.classList.toggle('bg-success'))); 
 document.querySelectorAll(".edit-button").forEach(e => e.addEventListener('click', e => e.target.classList.toggle('bg-danger')));
