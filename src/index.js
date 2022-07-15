405f1c53fd1f67f8d04a60026a8f3e8a3df98d51
Elena Kovalenko <aszhigalova@gmail.com>
2022-07-15 20:26:47 +0300

    added pagination for photo container which does not work

--- src/index.js
+++ src/index.js
@@ -8,39 +8,119 @@ const photos = [
   {url: "/img/images (7).jfif", name: "photo 7"},
   {url: "/img/images (8).jfif", name: "photo 8"},
   {url: "/img/images (9).jfif", name: "photo 9"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (7).jfif", name: "photo 7"},
+  {url: "/img/images (9).jfif", name: "photo 9"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (11).jfif", name: "photo 4"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (9).jfif", name: "photo 9"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (7).jfif", name: "photo 7"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (11).jfif", name: "photo 4"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (9).jfif", name: "photo 9"},
+  {url: "/img/images (7).jfif", name: "photo 7"},
+  {url: "/img/images (3).jfif", name: "photo 3"},
+  {url: "/img/images (11).jfif", name: "photo 4"},
 ];
 
-const addNewPhoto = (photo) => {
-      const div = document.createElement("div");
-      div.className = "template-card col-3 my-1";
-      div.innerHTML = '<div class="bg-light">' +
-    '<div class="card bg-light p-1 border rounded-3">' +
-    '   <img class="img-item rounded-3">' +
-    '     <div class="card-body">' +
-    '       <p class="card-text"> </p>' +
-    '<div class="d-flex justify-content-between align-items-center">' +
-    '  <div class="btn-group">' +
-    '     <button type="button" class="view-button btn btn-sm btn-outline-secondary">View</button>' +
-    '     <button type="button" class="edit-button btn btn-sm btn-outline-secondary">Edit</button>' +
-    '  </div>' +
-    '    <small class="text-muted">9 mins</small>' +
-    '</div></div></div></div>';
-    div.querySelector("img").src = photo.url;
-    div.querySelector(".card-text").innerText = photo.name;
-    document.querySelector(".photo-container").appendChild(div);
-};
+// const addNewPhoto = (photo) => {
+//       const div = document.createElement("div");
+//       div.className = "template-card col-3 my-1";
+//       div.innerHTML = '<div class="bg-light">' +
+//     '<div class="card bg-light p-1 border rounded-3">' +
+//     '   <img class="img-item rounded-3">' +
+//     '     <div class="card-body">' +
+//     '       <p class="card-text"> </p>' +
+//     '<div class="d-flex justify-content-between align-items-center">' +
+//     '  <div class="btn-group">' +
+//     '     <button type="button" class="view-button btn btn-sm btn-outline-secondary">View</button>' +
+//     '     <button type="button" class="edit-button btn btn-sm btn-outline-secondary">Edit</button>' +
+//     '  </div>' +
+//     '    <small class="text-muted">9 mins</small>' +
+//     '</div></div></div></div>';
+//     div.querySelector("img").src = photo.url;
+//     div.querySelector(".card-text").innerText = photo.name;
+//     document.querySelector(".photo-container").appendChild(div);
+// };
 
-photos.forEach(addNewPhoto);
+// photos.forEach(addNewPhoto);
 
-document.querySelectorAll(".view-button").forEach(e => e.addEventListener('click', e => e.target.classList.toggle('bg-success'))); 
-document.querySelectorAll(".edit-button").forEach(e => e.addEventListener('click', e => e.target.classList.toggle('bg-danger')));
+// document.querySelectorAll(".view-button").forEach(e => e.addEventListener('click', e => e.target.classList.toggle('bg-success'))); 
+// document.querySelectorAll(".edit-button").forEach(e => e.addEventListener('click', e => e.target.classList.toggle('bg-danger')));
 
-const viewPhoto = (event) => {
-  const target = event.target;
+// document.querySelectorAll(".img-item").forEach(e => e.addEventListener('click', e => e.target.classList.toggle("active"))); 
 
-  if (target.classList.contains('img-item')) {
-    console.log('click');
+// Pagination
+
+let current_page = 1;
+let records_per_page = 8;
+
+function prevPage() {
+  if (current_page > 1) {
+      current_page--;
+      changePage(current_page);
   }
-};
+}
 
-document.querySelectorAll(".img-item").forEach((e) => e.addEventListener('click', viewPhoto)); 
+function nextPage() {
+  if (current_page < numPages()) {
+      current_page++;
+      changePage(current_page);
+  }
+}
+    
+function changePage(page) {
+  let btn_next = document.querySelector(".btn_next");
+  let btn_prev = document.querySelector(".btn_prev");
+  let page_span = document.querySelector(".page");
+
+  if (page < 1) page = 1;
+  if (page > numPages()) page = numPages();
+
+  document.querySelector(".listingTable").innerHTML = "";
+
+  for (let i = (page-1) * records_per_page; i < (page * records_per_page) && i < photos.length; i++) {
+    const div = document.createElement("div");
+    div.className = "template-card col-3 my-1";
+    div.innerHTML = '<div class="bg-light">' +
+  '<div class="card bg-light p-1 border rounded-3">' +
+  '   <img class="img-item rounded-3">' +
+  '     <div class="card-body">' +
+  '       <p class="card-text"> </p>' +
+  '<div class="d-flex justify-content-between align-items-center">' +
+  '  <div class="btn-group">' +
+  '     <button type="button" class="view-button btn btn-sm btn-outline-secondary">View</button>' +
+  '     <button type="button" class="edit-button btn btn-sm btn-outline-secondary">Edit</button>' +
+  '  </div>' +
+  '    <small class="text-muted">9 mins</small>' +
+  '</div></div></div></div>';
+  div.querySelector("img").src = photos[i].url;
+  div.querySelector(".card-text").innerText = photos[i].name;
+  document.querySelector(".photo-container").appendChild(div);
+  }
+
+  page_span.innerHTML = page + "/" + numPages();
+
+  if (page == 1) {
+      btn_prev.style.visibility = "hidden";
+  } else {
+      btn_prev.style.visibility = "visible";
+  }
+
+  if (page == numPages()) {
+      btn_next.style.visibility = "hidden";
+  } else {
+      btn_next.style.visibility = "visible";
+  }
+}
+
+function numPages() {
+    return Math.ceil(photos.length / records_per_page);
+}
+
+window.onload = function() {
+    changePage(1);
+}
