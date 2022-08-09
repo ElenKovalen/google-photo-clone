36a18ddeae29e365bfe71acace7f5d790a110e00
Elena Kovalenko <aszhigalova@gmail.com>
2022-08-09 22:02:37 +0300

    big view for image added

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const photoContainer = document.querySelector(".photo-container");
 const options = { method: 'GET' };
 
 const addPhotos = async function() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/photos/', options);
+  const response = await fetch('http://jsonplaceholder.typicode.com/photos', options);
   const data = await response.json();
   const images = data.map(photo => new Image(photo))
     for (let i = 1; i < 6; i++) {
